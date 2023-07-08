5aff514c91530ce1566d10d58cc5342c24661c3c
Simón Oroño <simonorono@protonmail.com>
2023-07-08 15:17:08 -0400

    Use defined constant instead of literal

--- src/problems/1-9/ex9.h
+++ src/problems/1-9/ex9.h
@@ -9,7 +9,7 @@ void ex9() {
   for (int i = 1; i < EX9_MAX; ++i) {
     for (int j = 1; j < EX9_MAX; ++j) {
       for (int k = 1; k < EX9_MAX; ++k) {
-        if (i + j + k == 1000 && (i * i + j * j == k * k)) {
+        if (i + j + k == EX9_MAX && (i * i + j * j == k * k)) {
           printf("%d\n", i * j * k);
           return;
         }
