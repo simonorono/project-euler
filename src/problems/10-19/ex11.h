49c3a39c11f84bd6714f11403ad1aafafda416e1
Simón Oroño <simonorono@protonmail.com>
2023-07-08 19:45:18 -0400

    Add solution for problem #11

---
+++ src/problems/10-19/ex11.h
@@ -0,0 +1,6 @@
+#ifndef PROJECT_EULER_EX11_H
+#define PROJECT_EULER_EX11_H
+
+void ex11();
+
+#endif//PROJECT_EULER_EX11_H
--- src/problems/10-19/exercises.h
+++ src/problems/10-19/exercises.h
@@ -2,7 +2,8 @@
 #define PROJECT_EULER_EXERCISES_10_19_H
 
 #include "ex10.h"
+#include "ex11.h"
 
-#define EX_10_19 ex10
+#define EX_10_19 ex10, ex11
 
 #endif//PROJECT_EULER_EXERCISES_10_19_H
