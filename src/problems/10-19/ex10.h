6d103c65abca68a25d558a515b3be33055db120d
Simón Oroño <simonorono@protonmail.com>
2023-07-08 15:14:14 -0400

    Reduce the stack size after Valgrind complaints

--- src/problems/10-19/ex10.h
+++ src/problems/10-19/ex10.h
@@ -8,13 +8,15 @@
 #define EX10_MAX 2'000'000
 
 void ex10() {
-  uint8_t sieve[EX10_MAX];
+  uint8_t *sieve = calloc(sizeof(uint8_t), EX10_MAX);
   init_sieve(sieve, EX10_MAX);
   long sum = 0;
 
   for (int i = 0; i < EX10_MAX; ++i)
     if (is_prime(sieve, i))
       sum += i;
+
+  free(sieve);
 
   printf("%ld\n", sum);
 }
