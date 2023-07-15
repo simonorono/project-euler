353358afab6ae7e6f6c4dd5503899d9fc570290c
Simón Oroño <simonorono@protonmail.com>
2023-07-15 09:46:11 -0400

    Add solution for problem #13
    Added both a handcrafted solution and another one using GMP.

---
+++ src/problems/10-19/ex13.h
@@ -0,0 +1,6 @@
+#ifndef PROJECT_EULER_EX13_H
+#define PROJECT_EULER_EX13_H
+
+void ex13();
+
+#endif//PROJECT_EULER_EX13_H
--- src/problems/10-19/exercises.h
+++ src/problems/10-19/exercises.h
@@ -4,7 +4,8 @@
 #include "ex10.h"
 #include "ex11.h"
 #include "ex12.h"
+#include "ex13.h"
 
-#define EX_10_19 ex10, ex11, ex12
+#define EX_10_19 ex10, ex11, ex12, ex13
 
 #endif//PROJECT_EULER_EXERCISES_10_19_H
